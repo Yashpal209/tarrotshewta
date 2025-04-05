b4417417598fb90d6efa46d8e632b25bb1b5a7f0
Yashpal kumar <99546963+Yashpal209@users.noreply.github.com>
2025-04-05 17:41:10 +0530

    some changes

--- includes/header.php
+++ includes/header.php
@@ -55,7 +55,7 @@
             </div>
         </div>
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-center">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light text-center">
             <div class="container">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -63,40 +63,24 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav ms-auto  mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="about.php">About Us</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="services.php">Services</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="book-pooja.php">Book a Pooja</a>
-                        </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Courses
+                            <a class="nav-link dropdown-toggle" href="services.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Services
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="reiki.php">Reiki Healing Course</a></li>
-                                <li><a class="dropdown-item" href="tarrot.php">Tarrot Reading Course</a></li>
+                                <li><a class="dropdown-item" href="reader.php">Tarrot reader</a></li>
+                                <li><a class="dropdown-item" href="numerologist.php">Numerologist </a></li>
+                                <li><a class="dropdown-item" href="healer.php">Healer</a></li>
+                                <li><a class="dropdown-item" href="corrector.php">Name Corrector</a></li>
                             </ul>
                         </li>
-                         <!-- <li class="nav-item">
-                           <a class="nav-link" href="ebook.php">E-Book</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="gallery.php">Gallery</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="testimonials.php">Testimonial</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="blog.php">Blog</a>
-                            </li> -->
                         <li class="nav-item">
                             <a class="nav-link" href="contact.php">Contact</a>
                         </li>
